ce4bf38441210207800c834ec47737894f3d7b89
Anik Reza <anikreza007@gmail.com>
2023-10-08 21:17:30 +0600

    accounts views updeted

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def forget_password(request):
@@ -10,4 +12,27 @@ def sign_in(request):
 
 
 def sign_up(request):
+    if request.method == "POST":
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+
+        if len(password) == 0 and len(password1) == 0:
+            messages.warning(request, "Input Password.!")
+        else:
+            if password == password1:
+                if User.objects.filter(username=username).exists():
+                    messages.warning(request, "Username Already Taken.!")
+                elif User.objects.filter(email=email).exists():
+                    messages.warning(request, "Email Already Taken.!")
+                else:
+                    user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username, email=email, password=password)
+                    user.set_password(password)
+                    user.save()
+                    messages.success(request, "User Created.")
+                    return redirect('signin')
+
     return render(request, 'accounts/signup.html')
